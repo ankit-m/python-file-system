4c8961b581492972d2abadc0bfeedbc3ca55098a
Ankit Muchhala <muchhalaankit@gmail.com>
2016-04-16 23:30:28 +0530

    add git ignore

--- filesystem.py
+++ filesystem.py
@@ -1,20 +1,52 @@
-def create_file(arg):
-    pass
+from harddisk import disk
+import pickle
 
-def open_file(arg):
-    pass
 
-def read_file(arg):
-    pass
+def traverse_disk(path):
+    directories = path.split('/')
+    directories.pop()
+    location = disk[directories[0]]
+    for folder in directories[1:]:
+        location = location[folder]
+    return location
+
+
+def open_file(path, args):
+    location = traverse_disk(path)
+    filename = args[0]
+    filetype = args[0].split('.')[0]
+    location[filename] = {
+        'type': filetype,
+        'attrs': 'rw',
+        'data': ' '
+    }
+
+
+def read_file(path, args):
+    location = traverse_disk(path)
+    filename = args[0]
+    try:
+        print location[filename]['data']
+    except KeyError:
+        print 'No such file in this directory'
+
 
 def write_file(arg):
     pass
 
-def delete_file(arg):
-    pass
+
+def delete_file(path, args):
+    location = traverse_disk(path)
+    filename = args[0]
+    try:
+        del location[filename]
+    except KeyError:
+        print 'No such file in this directory'
+
 
 def get_attributes(arg):
     pass
+
 
 def set_attributes(arg):
     pass
--- main.py
+++ main.py
@@ -2,4 +2,17 @@ import filesystem
 import dirsystem
 import os
 
-# get cli arguments
+commands = {
+    'open': filesystem.open_file,
+    'read': filesystem.read_file,
+    'delete': filesystem.delete_file
+}
+
+current_path = 'root/'
+
+while True:
+    inp = raw_input(current_path + ' > ')
+    try:
+        commands[inp.split(' ')[0]](current_path, inp.split(' ')[1:])
+    except KeyError:
+        print 'Invalid Command'
